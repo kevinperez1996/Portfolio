495ebc578a97f866528e18837637273304930099
Kevin Perez <kevinperez996@outlook.com>
2019-10-09 21:07:49 -0400

    added some final logic. Still need to modify buttons and images

--- assets/Javascript/code.js
+++ assets/Javascript/code.js
@@ -1,5 +1,45 @@
 
-$(".projects-btn").on("click", function(event){
+var firebaseConfig = {
+    apiKey: "AIzaSyCjikVm2mF1FHvUfaXe7rIzs_HnAAADOr8",
+    authDomain: "portfolio-comments-90836.firebaseapp.com",
+    databaseURL: "https://portfolio-comments-90836.firebaseio.com",
+    projectId: "portfolio-comments-90836",
+    storageBucket: "portfolio-comments-90836.appspot.com",
+    messagingSenderId: "1038243048434",
+    appId: "1:1038243048434:web:61a8054a86c48cecb1596b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+$(".comments-btn").on("click", function (event) {
+
+if ($(".comments-box").val() === ""){
+    alert("Please fill in a comment");
+}
+else {
+    var comments = $(".comments-box").val().trim();
+
+    event.preventDefault();
+    
+    $(".comments-box").val("");
+
+    database.ref().push(comments);
+
+    var gracias = $("<div>").append (
+
+     $("<p>").text("Thanks for your feedback!")
+    );
+
+    $("#thanks").append(gracias);
+}
+});
+
+
+
+$(".projects-btn").on("click", function (event) {
     event.preventDefault()
     $(".more-projects").empty();
 
@@ -15,7 +55,9 @@ $(".projects-btn").on("click", function(event){
         $("<br>"),
         $("<a class = 'list' href='https://kevinperez1996.github.io/Unit-4-game/' target='_blank'>").text("Crystal Collectors"),
         $("<br>"),
+        $("<a class = 'list' href='https://kevinperez1996.github.io/Word-Guess-Game/' target='_blank'>").text("Artist Guessing Game"),
+        $("<br>"),
     );
 
     $(".more-projects").append(projects);
-})
+});
