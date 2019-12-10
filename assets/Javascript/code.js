0809aa087dfbf938994591ce18620b2ba753da26
Kevin Perez <kevinperez996@outlook.com>
2019-12-10 15:06:10 -0500

    updated Portfolio

--- assets/Javascript/code.js
+++ assets/Javascript/code.js
@@ -14,59 +14,37 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-$(".comments-btn").on("click", function (event) {
+$("#contactSubmitButton").on("click", function (event) {
+    event.preventDefault();
 
-if ($(".comments-box").val() === ""){
-    alert("Please fill in a comment");
+if ($("#contactName").val() === "" || $("#contactEmail").val() === "" ){
+    alert("Please fill in required fields");
 }
 else {
-    var comments = $(".comments-box").val().trim();
 
-    event.preventDefault();
+    var formInfo = [];
+    var contactName = $("#contactName").val().trim();
+    var contactEmail = $("#contactEmail").val().trim();
+    var contactComments = $("#contactComments").val().trim();
+
+
+    formInfo.push({
+        name: contactName,
+        email: contactEmail,
+        comments: contactComments
+    })
     
-    $(".comments-box").val("");
+    $("#contactName").val("");
+    $("#contactEmail").val("");
+    $("#contactComments").val("");
 
-    database.ref().push(comments);
-
+    database.ref().push(formInfo);
+   
     var gracias = $("<div>").append (
 
-     $("<p>").text("Thanks for your feedback!")
+     $("<p>").text("Thank you! your form had been submitted!")
     );
 
     $("#thanks").append(gracias);
 }
 });
-
-// $(".projects-btn").on("click", function (event) {
-//     event.preventDefault();
-//     $(".more-projects").empty();
-//     var projects = $("<lu>").append(
-
-//         // $("<a class = 'list' href='https://quiet-waters-80100.herokuapp.com/' target='_blank'>").text("Online Planner"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://still-escarpment-18133.herokuapp.com/burger' target='_blank'>").text("Burger Fest!"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://shrouded-brook-95001.herokuapp.com/' target='_blank'>").text("The Amazing Friend Finder"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://github.com/kevinperez1996/Bamazon-MySQL' target='_blank'>").text("Bamazon-MySQL"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://github.com/kevinperez1996/Liri-App' target='_blank'>").text("Liri Node Application"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://rrjethva.github.io/Eventbrite-Google-API/' target='_blank'>").text("Bravo Events - Group Project"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://kevinperez1996.github.io/Train-Schedule/' target='_blank'>").text("Train Scheduler"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://kevinperez1996.github.io/giphy/' target='_blank'>").text("Giphs' and giphs'"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://kevinperez1996.github.io/TriviaGame/' target='_blank'>").text("Automotive Trivia"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://kevinperez1996.github.io/Unit-4-game/' target='_blank'>").text("Crystal Collectors"),
-//         // $("<br>"),
-//         // $("<a class = 'list' href='https://kevinperez1996.github.io/Word-Guess-Game/' target='_blank'>").text("Artist Guessing Game"),
-//         // $("<br>"),
-        
-       
-//     );
-
-//     $(".more-projects").append(projects);
-// });
